51bd7fb989eb504a80359a9d919a2e4b734d2a73
Theotime Balaguer <theotime.balaguer@gmail.com>
2024-08-21 20:27:55 -0400

    fix absolute path in tmux script

--- src/tmux_scripts/start_dancers_viragh.sh
+++ src/tmux_scripts/start_dancers_viragh.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 session="sim"
+path_to_viragh='~/flocking-simulator'
+[ -n "$1" ] && path_to_viragh="$1"
 
-tmux new-session -d -s $session 'cd ~/gits/flocking-simulator && ./robotflocksim_main -obst obstacles/cosim_obstacles.default'
+path_to_ros2_ws='~/sim_ws'
+[ -n "$2" ] && path_to_ros2_ws="$2"
+
+tmux new-session -d -s $session "cd $path_to_viragh && ./robotflocksim_main -obst obstacles/cosim_obstacles.default"
 tmux set remain-on-exit on
 
 window=0
 tmux rename-window -t $session:$window 'dancers'
-tmux split-window -t $session:$window -v -d "cd ~/simulation_ws && ros2 run ns-3_sim ns-3_sim_adhoc_no_tap --ros-args -p config_file:=/home/theotime/simulation_ws/src/config/config_flocking_4.yaml"
-tmux split-window -t $session:$window -h "cd ~/simulation_ws && ros2 run coordinator coordinator --ros-args -p config_file:=/home/theotime/simulation_ws/src/config/config_flocking_4.yaml"
+tmux split-window -t $session:$window -v -d "cd $path_to_ros2_ws && ros2 run ns-3_sim ns-3_sim_adhoc_no_tap --ros-args -p config_file:=$path_to_ros2_ws/src/config/config_flocking_4.yaml"
+tmux split-window -t $session:$window -h "cd $path_to_ros2_ws && ros2 run coordinator coordinator --ros-args -p config_file:=$path_to_ros2_ws/src/config/config_flocking_4.yaml"
 tmux select-pane -t $session:$window.2
-tmux split-window -t $session:$window -h "cd ~/simulation_ws && ros2 run viragh_connector viragh_connector --ros-args -p config_file:=/home/theotime/simulation_ws/src/config/config_flocking_4.yaml"
+tmux split-window -t $session:$window -h "cd$path_to_ros2_ws && ros2 run viragh_connector viragh_connector --ros-args -p config_file:=$path_to_ros2_ws/src/config/config_flocking_4.yaml"
 
 tmux attach-session -t $session -d
